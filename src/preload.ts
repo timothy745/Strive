ae388c2d7c25da7c924b4f16e890a0711eaaf45a
timothy745 <timothydarren147258369@gmail.com>
2026-05-05 14:10:24 +0700

    V2.2

--- src/preload.ts
+++ src/preload.ts
@@ -1,10 +1,18 @@
 import { contextBridge, ipcRenderer } from 'electron';
- 
+
 contextBridge.exposeInMainWorld('striveAPI', {
   login: (email: string, password: string) =>
     ipcRenderer.invoke('login', { email, password }),
- 
+
   register: (email: string, password: string) =>
     ipcRenderer.invoke('register', { email, password }),
+
+  updateProfile: (data: any) =>
+    ipcRenderer.invoke('updateProfile', data),
+
+  getCurrentUser: () =>
+    ipcRenderer.invoke('getCurrentUser'),
+
+  uploadProfilePic: () =>
+    ipcRenderer.invoke('uploadProfilePic'),
 });
- 
